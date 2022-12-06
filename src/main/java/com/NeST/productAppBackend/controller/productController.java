bb0e0d04ed66a17acafa7ce5c07e94cbd09d22e9
Salooma <saloomaali2000@gmail.com>
2022-12-06 07:43:18 +0530

    new delete added

--- src/main/java/com/NeST/productAppBackend/controller/productController.java
+++ src/main/java/com/NeST/productAppBackend/controller/productController.java
@@ -1,5 +1,6 @@
 package com.NeST.productAppBackend.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +19,16 @@ public class productController {
     public String productSearch(){
         return "welcome to product search page";
     }
-    @PostMapping("/view")
+    @PostMapping("/edit")
+    public String productEdit(){
+        return "welcome to product edit page";
+    }
+    @GetMapping("/view")
     public String productView(){
         return "welcome to product view page";
+    }
+    @PostMapping("/delete")
+    public String productdDelete(){
+        return "welcome to product delete page";
     }
 }
