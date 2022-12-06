2e28f0f3942b36cb8f46d970dce8bb3842e1cb4e
Salooma <saloomaali2000@gmail.com>
2022-12-06 07:27:24 +0530

    new backend project created

---
+++ src/main/java/com/NeST/productAppBackend/ProductAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.NeST.productAppBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/NeST/productAppBackend/controller/productController.java
@@ -0,0 +1,13 @@
+package com.NeST.productAppBackend.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class productController {
+
+    @PostMapping("/")
+    public String productWelcome(){
+        return "welcome to product website";
+    }
+}
